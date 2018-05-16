64663c71861506de8af8133ba8adfc963ea7fe4f
Joe Rivett <joe.rivett@songkick.com>
2018-05-16 21:34:03 +0100

    Add default data so demo yields some recommendations results

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,3 +1,10 @@
+require_relative '../services/yaypi'
+
 class DiscoverController < ApplicationController
-  
+  def index
+    friends = ['ablinov', 'us_kids_know']
+    user = User.new('rivett', friends)
+
+    @recommendations = user.recommendations
+  end
 end
