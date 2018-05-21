0dd5e8de1b7e4808e98a7d41473b8110cf9fa206
Joe Rivett <joe.rivett@songkick.com>
2018-05-21 07:22:55 +0100

    Delete unused Rails boilerplate files

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,7 +2,7 @@ require_relative '../services/yaypi'
 
 class DiscoverController < ApplicationController
   def index
-    friends = ['ablinov', 'us_kids_know']
+    friends = ['rivett']
     user = User.new('rivett', friends)
 
     @recommendations = user.recommendations
